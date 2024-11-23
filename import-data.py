1e2a385bb5163e72133a0438cd7c03f5346e6bae
schen108 <schen9@arizona.edu>
2024-11-23 19:48:07 +0000

    modified file to import datasests

---
+++ import-data.py
@@ -0,0 +1,36 @@
+command = ["pip3", "instsall", "kagglehub"]
+result = subprocess.run(command, capture_output=True, text=True)
+
+
+import kagglehub
+import subprocess
+
+github = "/home/jovyan/data-store/CSC-396-Gaming-Toxicity"
+
+# Dota dataset
+path = kagglehub.dataset_download("danielfesalbon/gosu-ai-english-dota-chat")
+print("Path to Dota dataset files:", path)
+
+command = ["mv", path, github]
+result = subprocess.run(command, capture_output=True, text=True)
+print(f"Moved files from {path} to {github}")
+
+print(result.stdout)
+
+if result.stderr:
+    print("Errors:")
+    print(result.stderr)
+
+
+path = kagglehub.dataset_download("simshengxue/league-of-legends-tribunal-chatlogs")
+print("Path to League dataset files:", path)
+
+command = ["mv", path, github]
+result = subprocess.run(command, capture_output=True, text=True)
+print(f"Moved files from {path} to {github}")
+
+print(result.stdout)
+
+if result.stderr:
+    print("Errors:")
+    print(result.stderr)
--- testing.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("simshengxue/league-of-legends-tribunal-chatlogs")
-
-print("Path to dataset files:", path)
